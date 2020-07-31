106176f2317024210c32f7d145562a862b1abf86
Velu S Gautam <i@velusgautam.com>
2020-07-31 22:15:03 +0530

    Fix rerendering bug after completed

--- src/get-time.ts
+++ src/get-time.ts
@@ -1,19 +1,37 @@
-const getTimeLeft = (countDownDate: number | string | Date) => {
-  let startTimestamp: number
+interface Time {
+  completed: boolean
+  days: number
+  hours: number
+  minutes: number
+  seconds: number
+  milliseconds: number
+  total: number
+}
 
+/**
+ * Get Time Left
+ * @param {Date|string|number} countDownDate
+ */
+const getTimeLeft = (countDownDate: number | string | Date): Time => {
+  let startTimestamp: number
+  // If string Date converting to milliseconds
   if (typeof countDownDate === 'string') {
     startTimestamp = new Date(countDownDate).getTime()
   } else if (countDownDate instanceof Date) {
+    // getting milliseconds from Date object
     startTimestamp = countDownDate.getTime()
   } else {
+    // setting the date in milliseconds
     startTimestamp = Number(countDownDate)
   }
 
+  // getting the date into total seconds
   const total = Math.round(
     parseFloat((Math.max(0, startTimestamp - Date.now()) / 1000).toFixed(0)) *
       1000
   )
 
+  // if total is NaN then the time format has error
   if (isNaN(total)) {
     throw new TypeError('Error in date format passed to Count Down Component')
   }
--- src/index.tsx
+++ src/index.tsx
@@ -2,25 +2,40 @@ import { useState, useEffect } from 'react'
 import getTimeLeft from './get-time'
 import { CounterProps } from './index.types'
 
-const CountDown = ({ date, renderer, updateFrequency }: CounterProps) => {
+/**
+ * Count Down Render Prop Component
+ * @param {Object} props - Count Down properties
+ * @param {Date|string|number} props.date - Starting Date for count down
+ * @param {function} props.renderer - Render function
+ * @param {function} props.updateFrequency - Update frequency function
+ */
+const CountDown = ({
+  date,
+  renderer,
+  updateFrequency,
+}: CounterProps): React.Component => {
+  // throw error if renderer is not of type function
   if (typeof renderer !== 'function') {
     throw new TypeError('Renderer Function is Expected')
   }
-  if (typeof date !== 'number' || date < 0) {
-    console.error('Positive Date value expected', date)
-  }
 
   const [counter, setCounter] = useState(getTimeLeft(date))
+  // setting default update time to 1 seconds
   let updateTimer = 1000
+
   if (typeof updateFrequency === 'function') {
     updateTimer = updateFrequency(counter)
   }
+
   useEffect(() => {
-    const interval = setInterval(() => {
-      setCounter(getTimeLeft(date))
-    }, updateTimer)
-    return () => clearInterval(interval)
-  }, [date, updateTimer])
+    if (!counter.completed) {
+      const interval = setInterval(() => {
+        setCounter(getTimeLeft(date))
+      }, updateTimer)
+      return () => clearInterval(interval)
+    }
+    return
+  }, [date, updateTimer, counter.completed])
 
   return renderer(counter)
 }
